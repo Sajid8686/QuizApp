d23c668c1dfa7fb992349101404ac6b4935348db
Sajid Hussain <sajid6207116@gmail.com>
2024-03-06 16:52:15 +0500

    question by id accesssed

--- index.js
+++ index.js
@@ -6,12 +6,43 @@ import data from "./data/quizdata.json" assert{type: "json"}
 function Input() {
     MainMenu()
     let input = prompt()
-    let idno = parseInt(input("Enter id number: "))
+    let idno = parseInt(input("Please Enter Serial Number: "))
     if (idno==1) {
         //console.log(data);
         allQuestions()
-        
-    } else {
+        Input()
+ 
+// *************************** Fetch question by ID Number ********************************        
+
+    } else if(idno==2){
+        let questionId = parseInt(input("Enter ID number of Specific Question: "))
+        let found =false
+        for (let i = 0; i < data.length; i++) { 
+            if (questionId==data[i].id) {
+                found = true
+                let optno = ['1', '2', '3', '4']
+                console.log("Q."+ i + ": (id:"+data[i].id+") "+ data[i].statement);
+                for(let j=0; j<data[i].options.length; j++){
+                    let alphabet = optno[j]
+                    if(data[i].answer == data[i].options[j]){
+                        console.log("\t"+ alphabet+ ". *"+ data[i].options[j])
+                    }
+                    else {
+                        console.log("\t"+ alphabet+ ". "+ data[i].options[j])
+                    }
+                }
+                Input()
+                
+            } 
+            // else{
+            //     console.log("Enter Valid ID");
+            // }
+        }
+        if(!found){
+            console.log("Enter Valid ID");
+        }
+    }
+    else {
         console.log("Thank you for Using this App");
         process.exit();
     }
@@ -49,8 +80,6 @@ function allQuestions() {
         }
     }
 }
-
-
 
 
 Input()
